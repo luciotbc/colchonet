7838403ac832f4c7a270b36c9a2676451c21f3fe
Lúcio Charallo <luciotbc@gmai.com>
2016-10-07 17:56:47 -0300

    add cryptography to user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
+    validates_presence_of :email, :full_name, :location
+    validates_length_of :bio, minimum: 30, allow_blank: false
+    validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+    validates_uniqueness_of :email
 
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
-  validates_length_of :bio, :minimum => 30, :allow_blank => false
-  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-  validates_uniqueness_of :email
-
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006190659) do
+ActiveRecord::Schema.define(version: 20161007203442) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20161006190659) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
